ce0b056f72d6917f58bc34bfb6168d3588054797
Oli <OliverWDGarrett@gmail.com>
2023-10-05 22:33:37 +1300

    Revert "hopefully centered divs"
    This reverts commit 4303a368e66e8e730735bfa36f13683d38985b79.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,28 +7,20 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const style = `
-      .centered {
-        margin: auto;
-      }
-  `
   return (
     <header class="homepage" className={clsx('hero hero--primary', styles.heroBanner)}>
-      <style>{style}</style>
       <div className="container">
-        <div class="centered">
-          <img src="img/title_txt_wide.png" class=".title-img"></img>
-        </div>
+        <img src="img/title_txt_wide.png" class=".title-img"></img>
         <br/>
         <br/>
 
-        <div class="centered">
+        <div>
           <a href="https://steampowered.com">
             <img src="img/play_free_steam.png"></img>
           </a>
         </div>
 
-        <div class="centered">
+        <div>
           <a href="https://steampowered.com">
             <img src="img/join_discord.png"></img>
           </a>
